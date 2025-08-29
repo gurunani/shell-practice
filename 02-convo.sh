a091b40c59fdfab14b5ad38b3d240b572e70b83e
Guru <nainiguruvinder@gmail.com>
2025-08-29 12:36:46 +0530

    hello world started

---
+++ 02-convo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "AI :: Hi guru , how are you ?"
+echo "Guru :: I am fine , how are you ?"
+echo "AI :: I am fine too , what are you doing ?"
+echo "Guru :: I am learning shell scripting."
+echo "AI :: That's great , keep learning and keep growing."
+echo "Guru :: Thank you AI , bye !"
