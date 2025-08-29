2e918f4455e89366810bffd81047c61b5e55a176
Guru <nainiguruvinder@gmail.com>
2025-08-29 22:37:58 +0530

    recovered from back

--- 12-special-var.sh
+++ 12-special-var.sh
@@ -2,3 +2,10 @@
 
 echo " all variables passed to script : $@"
 echo " number of var : $#"
+echo "script name $0"
+echo "pwd : $PWD"
+echo "home directory : $HOME"
+echo "user name : $USER"
+echo "process ID : $$"
+sleep 5 &
+echo " process id in back ground : $!
