68404688960c5c0939fc506ba47ce617e176a37a
Guru <nainiguruvinder@gmail.com>
2025-08-29 21:36:58 +0530

    recovered from back

---
+++ 06-variables-new.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1 :: Hi $person2 , how are you ?"
+echo "$person2 :: I am fine , how are you ?"
+echo "$person1 :: I am fine too , what are you doing ?"
+echo "$person2 :: I am learning shell scripting."
+echo "$person1 :: That's great , keep learning and keep growing."
+echo "$person2 :: Thank you $person1 , bye !"
