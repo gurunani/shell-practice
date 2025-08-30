3ac8d51bace86317080cf0ea2aa4dffdf2ec4714
Guru <nainiguruvinder@gmail.com>
2025-08-30 08:55:09 +0530

    new page

--- 15-install-own-test.sh
+++ 15-install-own-test.sh
@@ -10,15 +10,23 @@ else
     echo "you are running with root access"
 fi
 
-dnf list install mysql 
+dnf list installed mysql 
 
 if [ $? -eq 0 ]
 then 
-    echo "mysql installing success"
+    echo "mysql successfully installed"
     exit 0
 else
-    echo "mysql installing failed"
-    exit 1
+    echo "mysql is not installed, going to install now"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then 
+        echo "installing mysql is successful "
+        exit 0
+    else
+        echo "installing mysql is unsuccessful"
+        exit 1
+    fi
 fi
 
 echo " is exit code working"
