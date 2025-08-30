8006b28cece5b7989f5a936247ef6af16d789f09
Guru <nainiguruvinder@gmail.com>
2025-08-30 08:06:56 +0530

    new page

--- 14-installing-command.sh
+++ 14-installing-command.sh
@@ -8,5 +8,15 @@ then
     echo exit 1
 else
     echo "you are running with root acess"
+fi
+
+dnf install nginx -y
+
+if [ $? -eq 0 ]
+then 
+    echo "nginx installing sucess"
     exit 0
+else
+    echo "nginx installing faild"
+    exit 1
 fi
