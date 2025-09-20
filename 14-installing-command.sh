30e212138b42efb3133b0618d02144f476cac418
Guru <nainiguruvinder@gmail.com>
2025-09-20 17:34:58 +0530

    shell practice

--- 14-installing-command.sh
+++ 14-installing-command.sh
@@ -15,7 +15,6 @@ dnf install nginx -y
 if [ $? -eq 0 ]
 then 
     echo "nginx installing sucess"
-    exit 0
 else
     echo "nginx installing faild"
     exit 1
---
+++ 23-xml-json-practice.sh
@@ -0,0 +1,16 @@
+# PRACTICE
+<Student>
+    <Firstnsme>Guruvinder</Firstname>
+    <Lastname>Naini</Lastname>
+    <DOB>2001-05-06</DOB>
+    <Adresses>
+        <permAddress>
+            <Doorno>1</Doorno>
+            <street>school</street>
+        </PermAddress>
+        <CurrentAddress>
+			<DoorNo>3-30</DoorNo>
+			<Street>road no 27</Street>
+		</CurrentAddress>
+    </Addresses>
+</Student>
